271b2bed34004c0f6d0b1ee886780f43fbddd6f9
Shmush Marat <marat.shmush@privatbank.ua>
2015-07-16 12:56:07 +0300

    fix

--- src/com/sender/library/HttpSigleton.java
+++ src/com/sender/library/HttpSigleton.java
@@ -1,7 +1,8 @@
 package com.sender.library;
 
-import org.apache.http.HttpResponse;
+import org.apache.http.*;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.GzipDecompressingEntity;
 import org.apache.http.conn.ConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
@@ -9,6 +10,7 @@ import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.HttpContext;
 
+import java.io.IOException;
 import java.security.KeyStore;
 
 /**
@@ -39,6 +41,22 @@ public class HttpSigleton {
                             return 60000;
                         }
                     });
+            senderClient.addResponseInterceptor(new HttpResponseInterceptor() {
+                public void process(final HttpResponse response, final HttpContext context) throws HttpException, IOException {
+                    HttpEntity entity = response.getEntity();
+                    Header encheader = entity.getContentEncoding();
+                    if (encheader != null) {
+                        HeaderElement[] codecs = encheader.getElements();
+                        for (int i = 0; i < codecs.length; i++) {
+                            if (codecs[i].getName().equalsIgnoreCase("gzip")) {
+                                Log.v(ChatDispatcher.TAG, " === gzip detected!");
+                                response.setEntity(new GzipDecompressingEntity(entity));
+                                return;
+                            }
+                        }
+                    }
+                }
+            });
         }
         return senderClient;
     }
@@ -47,7 +65,7 @@ public class HttpSigleton {
         if(cometClient == null) {
             HttpParams httpParameters = new BasicHttpParams();
             HttpConnectionParams.setConnectionTimeout(httpParameters, 20000);
-            HttpConnectionParams.setSoTimeout(httpParameters, 20000);
+            HttpConnectionParams.setSoTimeout(httpParameters, 60000);
             cometClient = (keyStore == null) ? new DefaultHttpClient(httpParameters) : new SHttpClient(httpParameters, keyStore);
             cometClient.setKeepAliveStrategy(
                     new ConnectionKeepAliveStrategy() {
@@ -57,7 +75,24 @@ public class HttpSigleton {
                             return 60000;
                         }
                     });
+            cometClient.addResponseInterceptor(new HttpResponseInterceptor() {
+                public void process(final HttpResponse response, final HttpContext context) throws HttpException, IOException {
+                    HttpEntity entity = response.getEntity();
+                    Header encheader = entity.getContentEncoding();
+                    if (encheader != null) {
+                        HeaderElement[] codecs = encheader.getElements();
+                        for (int i = 0; i < codecs.length; i++) {
+                            if (codecs[i].getName().equalsIgnoreCase("gzip")) {
+                                Log.v(ChatDispatcher.TAG, " === gzip detected!");
+                                response.setEntity(new GzipDecompressingEntity(entity));
+                                return;
+                            }
+                        }
+                    }
+                }
+            });
         }
+
         return cometClient;
     }
 
@@ -74,6 +109,22 @@ public class HttpSigleton {
                         return 60000;
                     }
                 });
+        client.addResponseInterceptor(new HttpResponseInterceptor() {
+            public void process(final HttpResponse response, final HttpContext context) throws HttpException, IOException {
+                HttpEntity entity = response.getEntity();
+                Header encheader = entity.getContentEncoding();
+                if (encheader != null) {
+                    HeaderElement[] codecs = encheader.getElements();
+                    for (int i = 0; i < codecs.length; i++) {
+                        if (codecs[i].getName().equalsIgnoreCase("gzip")) {
+                            Log.v(ChatDispatcher.TAG, " === gzip detected!");
+                            response.setEntity(new GzipDecompressingEntity(entity));
+                            return;
+                        }
+                    }
+                }
+            }
+        });
         return client;
     }
 
